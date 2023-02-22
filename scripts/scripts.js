b7d47b42f76f2fdd65e7cc8649d97185bc93dc10
Harrison Sabean <92425655+Hsabes@users.noreply.github.com>
2023-02-21 20:25:59 -0500

    added expanding line animation to that lines up with transition of swiper

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,25 +1,9 @@
 $(document).ready(function() {
 
-
-    // CODE FOR ROTATING THE LAST WORD OF HTML TEXT
-
-    // function rotateWords() {
-    //     wordsArray = ['disc golfer.', 'cat dad.', 'guitarist.', 'developer.'];
-    //     count = 0;
-    //     setInterval(function () {
-    //         count++;
-    //         $('.shuffle').fadeOut(750, function () {
-    //             $(this).text(wordsArray[count % wordsArray.length]).fadeIn(750);
-    //         });
-    //     }, 4000);
-    // };
-
-    // rotateWords()
-
     $(window).on('scroll', () => {
         $scroll = $(window).scrollTop();
 
-        if ($scroll > 20) {
+        if ($scroll > 0) {
             $(".nav-wrap").addClass("shadow");
         } else {
             $(".nav-wrap").removeClass("shadow");
@@ -30,7 +14,7 @@ $(document).ready(function() {
         simulateTouch: false,
         speed: 2000,
         autoplay: {
-            delay: 4000,
+            delay: 16000,
         },
         slidesPerView: 1,
         loop: true,
