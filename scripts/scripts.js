d32407a6b705181aa015451444d5fbf3c440ae0c
Harrison Sabean <92425655+Hsabes@users.noreply.github.com>
2023-02-24 22:39:58 -0500

    added grid styling to skills and images as well

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,17 +9,17 @@ $(document).ready(function() {
       $('.shuffle-title').fadeOut(800, function () {
         $(this).text(titleArray[(count - 1) % titleArray.length]).fadeIn(800);
       });
-    }, 20000);
+    }, 2000);
   };
 
   rotateTitle();
 
 function rotateDesc(){
     let descArray = [
-        "When I'm not working, I spend a fair amount of my free time either solving Kata on CodeWars or writing some of my own. I do this so I'm consistently improving my ability to write creative custom code for unique problems.",
-        "In summer of 2022 my friend got me hooked on disc golfing. It's an incredible sport that facilitates social interaction, excercise, and an appreciation for the beauty of nature. My future plan is to travel the world and play some of the most famous courses around the globe.",
-        "I have a bachelor's degree in Guitar from Berklee College of Music, and have been playing the guitar since elementary school. My favorite genres to listen to are gypsie jazz, blues, EDM, and instrumental rock. I love playing BB King style blues on my Fender Strat.",
-        "I have a standard issue cat named Smitty who is currently four years old and never does anything wrong. He loves playing with wand toys, crinkling plastic (particularly at 3AM), and crab walking as an attempt to intimidate the vacuum cleaner.",
+        "When I'm not working or doing things in my spare time, I spend a fair amount of my free time either solving Kata on CodeWars or writing some of my own. I do this so I can consistently improve on my ability to write creative custom code for unique problems.",
+        "Currently my favorite hobby is to disc golf. It's an incredible sport that facilitates social interaction, exercise, and an appreciation for the beauty of nature. My future plan is to travel the world and play some of the most famous courses around the globe.",
+        "I have a bachelors degree in Guitar from Berklee College of Music and have been playing the guitar since elementary school. My favorite genres to listen to are EDM, blues, and instrumental rock and I love playing BB King style blues on my Fender Strat.",
+        "I have a standard issue cat named Smitty who is currently four years old and has never done anything or anyone wrong. He loves playing with his wand toys, crinkling plastic (particularly at 3AM), and crab walking as an attempt to intimidate the vacuum cleaner.",
         "I started coding in May of 2022 when I started studying at Flatiron School's Software Engineering program. I have a passion for finding creative solutions and I consistently exercise this passion through algorithms and project building. View my resume below."
     ];
     let count = 0;
@@ -28,7 +28,7 @@ function rotateDesc(){
         $('.shuffle-desc').fadeOut(800, function () {
             $(this).text(descArray[(count - 1) % descArray.length]).fadeIn(800);
         });
-    }, 20000);
+    }, 2000);
 }
 
 rotateDesc()
@@ -63,7 +63,7 @@ function animateScroll(){
         })
         $('html,body').animate({ // SCROLL DOWN TO GRID
             scrollTop: $(".skills-grid").offset().top
-        }, 1500);
+        }, 2000);
     });
     $(".to-intro").on('click', function() { // ON UP ARROW CLICK
         $('.hero').css({ // FADE IN HERO SECTION
@@ -74,7 +74,23 @@ function animateScroll(){
         }, 500)
         $('html,body').animate({ // SCROLL UP TO NAV
             scrollTop: $(".navigation").offset().top
-        }, 1500);
+        }, 2000);
+    });
+    $(".about-me").on('click', function() { // ON UP ARROW CLICK
+        $('.hero').css({ // FADE IN HERO SECTION
+            opacity: 100,
+        })
+        $('html,body').animate({ // SCROLL UP TO NAV
+            scrollTop: $(".hero").offset().top
+        }, 750);
+    });
+    $(".skills").on('click', function() { // ON UP ARROW CLICK
+        $('.skills-grid').css({ // FADE IN HERO SECTION
+            opacity: 100,
+        })
+        $('html,body').animate({ // SCROLL UP TO NAV
+            scrollTop: $(".skills-grid").offset().top
+        }, 750);
     });
 
   }
