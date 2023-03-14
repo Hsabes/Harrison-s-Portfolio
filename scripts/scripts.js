c11191ab16102f82ea9898e09d6e8943426cc88d
Harrison Sabean <92425655+Hsabes@users.noreply.github.com>
2023-03-13 23:02:38 -0400

    1.5.8
    Adjusted position of mobile navigation menu and added a border bottom

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -239,7 +239,7 @@ $(document).ready(function() {
         
         $('.about-me, .skills, .projects, .contact').on('click', function(){
             $('.mobile-nav').css({
-                top: '-100vh'
+                top: '-113vh'
             })
 
             $('.one').removeClass('rotatePos')
@@ -263,7 +263,7 @@ $(document).ready(function() {
                 }).addClass('is-active')
             } else {
                 $('.mobile-nav').css({
-                    top: '-100vh',
+                    top: '-113vh',
 
                 }).removeClass('is-active')
 
